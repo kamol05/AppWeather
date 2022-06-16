2dd16ecbbac32291d511710a2f06d30cabc25821
kamol05 <bestbrilliantboy@gmail.com>
2022-06-16 17:47:38 +0500

    Added Microservice for Agents

---
+++ src/test/java/com/example/appweather/AppWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
